7e2cf813ba12ce16ae8e6dcc33a6ad88f6e8f8af
PIpi123213 <1455076726@qq.com>
2024-05-15 03:42:31 +0800

    5.15 fuck

--- Assets/Script/Scene2/police.cs
+++ Assets/Script/Scene2/police.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class policeship : MonoBehaviour
+public class police : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Transform target;
+    private UnityEngine.AI.NavMeshAgent agent;
     void Start()
     {
-        
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.SetDestination(target.position);
     }
 }
